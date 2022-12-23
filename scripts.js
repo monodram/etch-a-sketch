ec6976cfccdc4acc9ed58d6f7297311b2744c2d1
monodram <dwiktorek11@gmail.com>
2022-12-23 19:04:53 +0100

    Created function that creates a grid

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 const grid = document.querySelector('.grid');
 
-let cellCount = 16;
+let cellCount = 256;
 
 function setGridSize(cellCount) { // creates a grid with choosen cell count
-
-
+    for (let i = 0; i < cellCount; i++) {
+        const createCells = document.createElement('div')
+        createCells.classList.add('cell');
+        grid.appendChild(createCells);
+    };
     
-    const createCells = document.createElement('div')
-    createCells.classList.add('cell');
-    grid.appendChild(createCells);
+
 
 }
