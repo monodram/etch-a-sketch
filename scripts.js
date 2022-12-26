cf9568711185f1e8ff10054c226c5d11cfc3b198
monodram <dwiktorek11@gmail.com>
2022-12-26 17:57:14 +0100

    Added slider

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ const gridContainer = document.querySelector('.grid-container');
 
 let cellCount = 64 * 64; // cell count aka grid size X by X
 
+// slider
+var slider = document.getElementById("sizeSlider");
+var output = document.getElementById("size-value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
 function setGridSize() { // creates a grid with selected cell count
     for (let i = 0; i < cellCount; i++) {
         const createCells = document.createElement('div')
@@ -24,3 +34,5 @@ const cells = document.querySelectorAll('.cell');
  });
 
 });
+
+
